d844b912f96c1249c89659498857fd77478c3b8d
Deltom <nogin-mikhaill@mail.ru>
2024-09-01 18:38:04 +0300

    Меню красиво уходит + функция по выводу красивого текста

--- Rogue/src/main/java/org/example/Menu.java
+++ Rogue/src/main/java/org/example/Menu.java
@@ -22,6 +22,8 @@ public class Menu
     Screen screen;
 
 
+
+
     public void callTerm() throws IOException   //Штука для создания консоли
     {
         terminal = defaultTerminalFactory.createTerminalEmulator();  //Создаём консоль
@@ -30,10 +32,8 @@ public class Menu
         screen.doResizeIfNecessary();
     }
 
-    public void createMenu() throws IOException
-    {
+    public void createMenu() throws IOException, InterruptedException {
         callTerm(); //Вызов терминала
-
 
         final TextGraphics textGraphics = terminal.newTextGraphics();   //Для текста
 
@@ -59,8 +59,54 @@ public class Menu
         KeyStroke keyStroke = terminal.readInput();
 
         terminal.flush();
+
+        int R = 169, G = 169, B = 169;  //Название затемняется
+        while (R > 0)
+        {
+            coolCloseName(R, G, B, text, 500);
+            if(R - 20 > 0)
+            {
+                R -= 20;
+                G -= 20;
+                B -= 20;
+            }
+            else
+            {
+                R = 0;
+                G = 0;
+                B = 0;
+            }
+        }
+
+        R = 255; G = 255; B = 255;
+        coolCloseName(R, G, B, "©JOYCAP", 10000);
+
         terminal.close();
     }
 
+    public void coolCloseName(int R, int G, int B, String text, int sleepTime) throws IOException, InterruptedException //Вывод разного текста во весь экран
+    {
+        final TextGraphics textGraphics = terminal.newTextGraphics();
+        String asciiArt = FigletFont.convertOneLine(text);
+
+        TerminalSize terminalSize = terminal.getTerminalSize();
+        int terminalWidth = terminalSize.getColumns();
+
+        // Разбиваем текст на строки и печатаем их по центру
+        String[] lines = asciiArt.split("\n");
+        int y = 4; // начальная строка для печати
+
+        terminal.clearScreen();
+
+        TextColor.RGB myColor = new TextColor.RGB(R, G, B);
+        textGraphics.setForegroundColor(myColor);
+        for (String line : lines) {
+            int textStartPosition = (terminalWidth - line.length()) / 2;
+            textGraphics.putString(textStartPosition, y++, line);
+        }
+        terminal.flush();
+        Thread.sleep(sleepTime);
+
+    }
 
 }
